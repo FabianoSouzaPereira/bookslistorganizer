568206f99fa696a2751eff1b886ac9aa54875439
Fabiano Pereira <jeovajire2003@gmail.com>
2021-03-18 17:54:13 -0300

    first commit

---
+++ app/src/main/java/br/com/bookslistoragnizer/adapters/RecyclerAdapter.java
@@ -0,0 +1,79 @@
+package br.com.bookslistoragnizer.adapters;
+
+import android.content.*;
+import android.view.*;
+import android.widget.*;
+import android.widget.ListAdapter;
+
+import androidx.annotation.*;
+import androidx.recyclerview.widget.*;
+
+import java.util.*;
+
+import br.com.bookslistoragnizer.*;
+import br.com.bookslistoragnizer.Models.*;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewholderBooks>
+   implements  View.OnClickListener, View.OnTouchListener, View.OnLongClickListener {
+   private View.OnClickListener clicklistener;
+   private View.OnTouchListener touchListener;
+   private View.OnLongClickListener longClickListener;
+   private List<Book> books;
+   private Context context;
+   
+   public RecyclerAdapter( List <Book> books){
+      this.books = books;
+   }
+   
+   public ViewholderBooks onCreateViewHolder(ViewGroup parent, int ViewType){
+      View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item2,parent,false );
+      view.setOnClickListener(this);
+      view.setOnTouchListener(this);
+      view.setOnLongClickListener(this);
+      return new ViewholderBooks(view);
+   }
+   
+   @Override
+   public void onClick( View view ){
+   
+   }
+   
+   @Override
+   public boolean onLongClick( View view ){
+      return false;
+   }
+   
+   @Override
+   public boolean onTouch( View view , MotionEvent motionEvent ){
+      return false;
+   }
+   
+   @NonNull
+   @Override
+   public ViewholderBooks onCreateViewHolder( @NonNull ViewGroup parent , int viewType ){
+      return null;
+   }
+   
+   @Override
+   public void onBindViewHolder( @NonNull ViewholderBooks holder , int position ){
+   
+   }
+   
+   @Override
+   public int getItemCount( ){
+      return 0;
+   }
+   
+   
+   //classe interna ViewholderBook
+   public class ViewholderBooks extends RecyclerView.ViewHolder {
+      EditText nome;
+   
+      public ViewholderBooks( @NonNull View itemView ){
+         super( itemView );
+         nome = itemView.findViewById( R.id.nome );
+      }
+   }
+   //FIm viewholder
+  
+}
